6891b08a66a2f9e3532b1c16f2fd329f1d2929e7
songguoguo927 <1351443467@qq.com>
2018-12-22 23:42:26 +0800

    web Worker demo finished

--- demo5/work.js
+++ demo5/work.js
@@ -1,10 +1,13 @@
 onmessage = function (e){
-             console.log("111");//控制台打印成功，说明数据传过来了
+            //  console.log("111");//控制台打印成功，说明数据传过来了
             let sum=0;
-            for(i=1;i<=e;i++){
-                console.log("判断有没有进来")
+            //  console.log(e);//在未计算前，打印出e是一个对象，看控制台发现，e.data才是我们要的数据n
+            for(i=1;i<=e.data;i++){
+                // console.log("判断有没有进来");//改成e.data后进来了
                  sum = sum +i; 
+                
             }
-            console.log(sum)
-            // postMessage(sum);
+            //  console.log(sum)//验证计算成功。将结果饭给主线程
+           
+            postMessage(sum);
         }
